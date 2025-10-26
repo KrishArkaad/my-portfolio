b5b05ec27fe0de6968a657d2bfdd08f2e8f1581d
Ajithkumar Krishnamoorthy <krisharkaad1997k@gmail.com>
2025-10-26 12:24:57 +0530

    removed repo name in th eurl

--- vite.config.js
+++ vite.config.js
@@ -37,11 +37,9 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7031';
 
-    const repoName = 'Portfolio';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: `/${repoName}/`,
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
