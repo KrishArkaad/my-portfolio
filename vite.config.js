03b6bf5b7118fec37b8228123c1c7a87f2fc2e27
Ajithkumar Krishnamoorthy <krisharkaad1997k@gmail.com>
2025-10-26 18:51:38 +0530

    base url changed

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: '../my-portfolio/',
+    base: "/my-portfolio/",
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
