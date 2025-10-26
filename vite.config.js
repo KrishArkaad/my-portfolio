91bcb8351f695a616bd8562405a94b1e2b29faf9
Ajithkumar Krishnamoorthy <krisharkaad1997k@gmail.com>
2025-10-26 15:46:33 +0530

    removed base url

--- vite.config.js
+++ vite.config.js
@@ -36,11 +36,10 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7031';
-    
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: '/my-portfolio/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
