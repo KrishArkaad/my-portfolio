bf40009813715bf9df003769006b0378ea7cb546
Ajithkumar Krishnamoorthy <krisharkaad1997k@gmail.com>
2025-10-26 15:39:06 +0530

    removed dollar symbol

--- vite.config.js
+++ vite.config.js
@@ -36,13 +36,11 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7031';
-
-    const repoName = 'my-portfolio';
-
+    
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: `$/${repoName}/`,
+    base: '/my-portfolio/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
